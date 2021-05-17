7a0e5979cfb9da13ff86b8b4b7058d85e2df9c40
LIME\Azarin.SA <azarin.sergey@gmail.com>
2021-05-17 18:17:44 +0700

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# FOR OTUS MENTOR:
+## How to
+1. [Run postgres (and all that is needed)](docs/infrastructure.md)
+2. Check homeworks:
+
+   * One
+   * Two
+   * Three 
+  
+
+3. I'm using sample db:
+   ![sample_db_schema](docs/content/db_schema.png)
+  
---
+++ build/deploy/local/postgres/bash/server_config.sh
@@ -0,0 +1,15 @@
+psql -U admin -d postgres -c "create user ${POSTGRES_USER_NAME} with createdb createrole PASSWORD '${POSTGRES_USER_PASSWORD}'"
+psql -U admin -d postgres -c "create database ${POSTGRES_USER_DB} owner=${POSTGRES_USER_NAME}"
+#
+#sample_db
+#
+psql -U admin -d postgres -c "create user postgres createdb createrole superuser PASSWORD '${POSTGRES_USER_PASSWORD}'"
+psql -U admin -d postgres -c "grant admin to postgres"
+psql -U postgres -d postgres -c "create database otus_dvdrental_db owner=postgres"
+pg_restore -U postgres -d otus_dvdrental_db -v /sample_db/dvdrental.tar --no-password
+#
+#execute sql
+#
+for file in /tmp/sql/*.sql; do 
+	psql -U ${POSTGRES_USER_NAME} -d ${POSTGRES_USER_DB} -a -f "${file}" 
+done
